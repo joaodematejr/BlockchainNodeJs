7bb95049cb01364d54593941a7302d5e2ba9d897
joaodematejr <joaodematejr@gmail.com>
2022-01-03 21:55:10 -0300

    Create Funcion MineBlock

--- block.js
+++ block.js
@@ -1,24 +1,38 @@
 class Block {
+  constructor(timestamp, lastHash, hash, data) {
+    this.timestamp = timestamp
+    this.lastHash = lastHash
+    this.hash = hash
+    this.data = data
+  }
 
-    constructor(timestamp, lastHash, hash, data) {
-        this.timestamp = timestamp;
-        this.lastHash = lastHash;
-        this.hash = hash;
-        this.data = data;
-    }
-
-    toString() {
-        return `Block -
+  toString() {
+    return `Block -
             Timestamp : ${this.timestamp}
             Last Hash : ${this.lastHash.substring(0, 10)}
             Hash      : ${this.hash.substring(0, 10)}
-            Data      : ${this.data}`;
-    }
+            Data      : ${this.data}`
+  }
 
-    static genesis() {
-        return new this('Genesis time', '-----', 'f1r57h45h', []);
-    }
-    
+    /* 
+    O que é Genesis em blockchain?
+    O Bloco Genesis, também conhecido como Bloco 0, 
+    é o primeiro bloco ao qual blocos adicionais em uma cadeia de blocos são adicionados . 
+    É efetivamente o ancestral ao qual todos os outros blocos podem rastrear sua linhagem, 
+    uma vez que cada bloco faz referência ao anterior. 
+    */
+
+  static genesis() {
+    return new this('Genesis time', '-----', 'f1r57h45h', [])
+  }
+
+  static mineBlock(lastBlock, data) {
+    const timestamp = Date.now()
+    const lastHash = lastBlock.hash
+    const hash = "a-fazer-hash"
+
+    return new this(timestamp, lastHash, hash, data)
+  }
 }
 
-module.exports = Block;
+module.exports = Block
--- test.js
+++ test.js
@@ -4,3 +4,9 @@ const block = new Block('15156', '121as2d1as2d1a2s1das', 'asdasdas10515115', '10
 
 console.log(block.toString());
 
+console.log(Block.genesis().toString());
+
+const firstBlock = Block.mineBlock(Block.genesis(), 'R$100');
+
+console.log(firstBlock.toString());
+
