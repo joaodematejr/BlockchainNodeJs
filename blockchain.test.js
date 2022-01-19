48dba8ed0532ec09df788b963817646263429f90
joaodematejr <joaodematejr@gmail.com>
2022-01-19 20:05:37 -0300

    Finish test validate a valid chain

--- blockchain.test.js
+++ blockchain.test.js
@@ -3,9 +3,11 @@ const Block = require('./block');
 
 describe('Blockchain', () => {
     let bc;
+    let bc2;
+
     beforeEach(() => {
         bc = new Blockchain;
-
+        bc2 = new Blockchain;
     });
 
     it('starts with the genesis block', () => {
@@ -16,5 +18,11 @@ describe('Blockchain', () => {
         data = "arquivo.pdf";
         bc.addBlock(data);
         expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
-    })
+    });
+
+    it('validates a valid chain', () => {
+        bc2.addBlock('R$500');
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+
+    });
 });
