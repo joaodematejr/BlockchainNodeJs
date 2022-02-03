e41a61b2f610efd84517c4f338f9d0cfee584eeb
joaodematejr <joaodematejr@gmail.com>
2022-02-02 22:58:16 -0300

    Update news functions

---
+++ app/p2p-server.js
@@ -0,0 +1,24 @@
+const WebSocket = require('ws')
+const P2P_PORT = process.env.P2P_PORT || 5001
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : []
+
+//HTTP_PORT = 3002 P2P_PORT = 5003 PEERS = "ws://localhost:5001,ws://localhost:5002" npm run dev
+
+class P2pServer {
+    constructor(blockchain) {
+        this.blockchain = blockchain
+        this.sockets = [];
+    }
+}
+
+listen() {
+    const server = new WebSocket.server({ port: P2P_PORT });
+    server.on('connection', socket => this.connectSocket(socket));
+    this.connectToPeers();
+    console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+}
+
+connectSocket(socket) {
+    this.sockets.push(socket);
+    console.log('Connected to a new peer: ' + socket.url);
+}
